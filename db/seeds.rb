428a72864182e2e202a7fa96980c0d9ae0f453c4
Arisa NAGAO <a-nagao@Gerhilde.local>
2018-01-29 19:28:48 +0900

    create seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,44 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+10.times do |n|
+  email = Faker::Internet.email
+  name = Faker::Name.name
+  password = "password"
+  User.create!(email: email,
+               password: password,
+               password_confirmation: password,
+               name: name,
+               uid: SecureRandom.uuid
+               )
+end
+
+
+n = 1
+comment_num = 1
+while n <= 10
+  content = Faker::Lorem.sentence
+  Post.create!(
+    content: content,
+    user_id: n
+  )
+  while comment_num <= 5
+    comment_content = Faker::Lorem.sentence
+    Comment.create!(
+      content: comment_content,
+      user_id: rand(10)+1,
+      post_id: n
+    )
+  comment_num = comment_num + 1
+  end 
+  n = n + 1
+  comment_num = 1
+end
+
+
+User.create!(email:"arisa@ntt",
+             password: "arisa1",
+             password_confirmation: "arisa1",
+             name: "Arisa",
+             uid: SecureRandom.uuid)
