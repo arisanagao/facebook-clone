54be34398193bee6845dd4e77d4f6131b3e4f441
Arisa NAGAO <a-nagao@Gerhilde.local>
2018-01-29 17:38:38 +0900

    change ipaddress

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,12 +77,12 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: '13.231.66.57' }
+  config.action_mailer.default_url_options = { host: '52.196.231.163' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings =
   { user_name: ENV['SENDGRID_USERNAME'],
     password: ENV['SENDGRID_PASSWORD'],
-    domain: "13.231.66.57",
+    domain: "52.196.231.163",
     address: "smtp.sendgrid.net",
     port: 587,
     authentication: :plain,
